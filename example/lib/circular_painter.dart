2f64ebe7857f9f1abf5b8d2c87a3d1634756b83d
mejdi14 <mejdiclubiste@gmail.com>
2021-07-30 21:06:58 +0100

    here we go again

---
+++ example/lib/circular_painter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CircularPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw UnimplementedError();
+    return true;
+  }
+
+}
