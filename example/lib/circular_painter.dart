3230e51202e1f4488507edc792f306d3c38001ef
mejdi14 <mejdiclubiste@gmail.com>
2021-07-31 23:10:39 +0100

    clean comments

--- example/lib/circular_painter.dart
+++ example/lib/circular_painter.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CircularPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    Paint circule =  Paint()
+        ..strokeWidth = 5
+        ..color = Colors.black
+        ..style = PaintingStyle.stroke;
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double radius = 150;
+    canvas.drawCircle(center, radius, circule);
   }
 
   @override
